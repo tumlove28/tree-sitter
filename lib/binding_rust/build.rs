029a90faf4f8abbd102c83d787c1b05d3ba7d928
Douglas Creager <dcreager@dcreager.net>
2023-11-20 09:39:19 -0500

    Include build script links in Rust bindings
    This patch updates the Rust binding's build script to output [build
    metadata][links].  This makes it easier for downstream crates to
    determine the include path, in case they need to compile their own C
    code that requires the tree-sitter headers.
    
    [links]: https://doc.rust-lang.org/cargo/reference/build-scripts.html#the-links-manifest-key

--- lib/binding_rust/build.rs
+++ lib/binding_rust/build.rs
@@ -27,7 +27,10 @@ fn main() {
         config.define("TREE_SITTER_FEATURE_WASM", "");
     }
 
-    let src_path = Path::new("src");
+    let manifest_path = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let include_path = manifest_path.join("include");
+    let src_path = manifest_path.join("src");
+    let wasm_path = src_path.join("wasm");
     for entry in fs::read_dir(&src_path).unwrap() {
         let entry = entry.unwrap();
         let path = src_path.join(entry.file_name());
@@ -39,11 +42,13 @@ fn main() {
         .flag_if_supported("-fvisibility=hidden")
         .flag_if_supported("-Wshadow")
         .flag_if_supported("-Wno-unused-parameter")
-        .include(src_path)
-        .include(src_path.join("wasm"))
-        .include("include")
+        .include(&src_path)
+        .include(&wasm_path)
+        .include(&include_path)
         .file(src_path.join("lib.c"))
         .compile("tree-sitter");
+
+    println!("cargo:include={}", include_path.display());
 }
 
 #[cfg(feature = "bindgen")]
